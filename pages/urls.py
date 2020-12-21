d2c7be9f07d94403ba3d00c5ed45c8fcd7dde28c
daffa haryadi <daffa.hp.dh@gmail.com>
2020-12-21 14:43:48 +0700

    some file

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpattern = [
+urlpatterns = [
     path('', views.home, name='home'),
-    path('about/',views.about, name='about'),
-    path()
+    path('about/',views.about, name='about')
         ]
 
